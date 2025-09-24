94e0f95f546133484069523cdaf000e350d43e23
Seth TAFIKA <tafikaseth@gmail.com>
2025-09-24 12:48:57 +0300

    feat: add menu burger dans navbar

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -70,6 +70,22 @@ const Navbar: React.FC = () => {
           <button className="hidden md:block bg-blue-500 hover:bg-blue-600 px-4 py-1 rounded-full text-white font-medium">
             Réservé
           </button>
+            <button className="ml-4 text-white hover:text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
         </div>
         <button
           className="md:hidden text-white"
